fd30ae8609da22be46b56e7cfd92b865653dd129
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-01 22:37:20 +0000

    safely get value

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module returns values, add type annotations to the function
+"""
+
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping[Hashable, T],
+    key: Hashable,
+    default: Union[T, None] = None
+) -> Union[T, None]:
+    """
+    Safely retrieves a value from a dictionary based on a key.
+
+    Args:
+        dct (Mapping[Hashable, T]): A dictionary-like mapping.
+        key (Hashable): The key.
+        default (Union[T, None], optional): The default value.
+
+    Returns:
+        Union[T, None]: The value associated with the key.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""
+This module uses mypy to validate the following piece of code and apply any necessary changes.
+"""
