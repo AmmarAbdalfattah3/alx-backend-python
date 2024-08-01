673821f686402fb166192c420342377c4e2891f4
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-01 22:43:28 +0000

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 """
-This module uses mypy to validate the following piece of code and apply any necessary changes.
+This module uses mypy to validate the following
+piece of code and apply any necessary changes.
 """
+
+
+from typing import List, Union
+
+
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    """
+    Zooms in on the elements of a list by repeating each element
+
+    Args:
+        lst (List[int]): The list of integers to be zoomed.
+
+    Returns:
+        List[int]: A new list with each element repeated 'factor' times.
+
+    Raises:
+        ValueError: If factor is not an integer.
+    """
+    if not isinstance(factor, int):
+        raise ValueError("Factor must be an integer.")
+
+    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+# Correctly pass an integer as factor
+zoom_3x = zoom_array(array, 3)
