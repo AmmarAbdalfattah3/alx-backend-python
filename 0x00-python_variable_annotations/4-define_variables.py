bfe5d3bb1669c6ab4125de6bb6af1b921736da41
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-01 20:22:31 +0000

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates variables with specified values.
+
+Variables:
+- a: int
+- pi: float
+- i_understand_annotations: bool
+- school: str
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
