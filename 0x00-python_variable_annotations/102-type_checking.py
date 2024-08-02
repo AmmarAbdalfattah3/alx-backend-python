98cbf7e1424cf3d082c0be6d38e034baaafd1079
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-02 23:30:53 +0000

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,33 +4,32 @@ This module uses mypy to validate the following
 piece of code and apply any necessary changes.
 """
 
+from typing import List, Sequence, Union
 
-from typing import List, Union
 
-
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+def zoom_array(lst: Sequence[Union[int, float]],
+               factor: int = 2) -> List[Union[int, float]]:
     """
-    Zooms in on the elements of a list by repeating each element
+    Zoom (duplicate) elements in the list based on the given factor.
 
     Args:
-        lst (List[int]): The list of integers to be zoomed.
+        lst (List[Union[int, float]]): A list of integers
+        or floats to be zoomed.
+        factor (int, optional): The factor by which to
+        duplicate the elements. Defaults to 2.
 
     Returns:
-        List[int]: A new list with each element repeated 'factor' times.
-
-    Raises:
-        ValueError: If factor is not an integer.
+        List[Union[int, float]]: A list with each element
+        duplicated `factor` times.
     """
-    if not isinstance(factor, int):
-        raise ValueError("Factor must be an integer.")
-
-    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
+    zoomed_in: List[Union[int, float]] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
     return zoomed_in
 
 
+# Sample usage
 array = [12, 72, 91]
-
 zoom_2x = zoom_array(array)
-
-# Correctly pass an integer as factor
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 30)
